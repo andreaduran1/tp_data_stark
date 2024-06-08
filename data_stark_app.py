3df635802731798d8efe2cf30f889e75285e44fc
andreaduran1 <134104457+andreaduran1@users.noreply.github.com>
2024-06-07 22:35:07 -0300

    se agrega menu

--- data_stark_app.py
+++ data_stark_app.py
@@ -18,6 +18,13 @@ def menu()->str:
     print("N. Superheroe más bajo de género F")
     print("O. Altura promedio de los superheroes de género M")
     print("P. Altura promedio de los superheroes de género F")
+    print("Q. Nombre del superheroe asociado a cada uno de los indicadores anteriores ítems K a N)")
+    print("R. Determinar cuántos superheroes tienen cada tipo de color de ojos)")
+    print("S. Determinar cuántos superhéroes tienen cada tipo de color de pelo")
+    print("T. Determinar cuántos superhéroes tienen cada tipo de inteligencia")
+    print("U. Listar todos los superhéroes agrupados por color de ojos")
+    print("V. Listar todos los superhéroes agrupados por color de pelo")
+    print("W. Listar todos los superhéroes agrupados por tipo de inteligencia")
     print("X. Salir\n")
 
 
@@ -46,12 +53,14 @@ def data_stark():
                 
             case "D":
                 heroe_mas_alto = reduce_lista(lambda ant, act: act if ant['altura'] < act['altura'] else ant, lista_personajes)
-                print(f"El superheroe más alto es {heroe_mas_alto}")
+                print(f"El superheroe más alto es: ")
+                mostrar_heroe(heroe_mas_alto)
                 print()
             
             case "E":
                 heroe_mas_bajo = reduce_lista(lambda ant, act: act if ant['altura'] > act['altura'] else ant, lista_personajes)
-                print(f"El superheroe más bajo es {heroe_mas_bajo}")
+                print(f"El superheroe más bajo es: ")
+                mostrar_heroe(heroe_mas_bajo)
                 print()
 
             case "F":
@@ -69,7 +78,92 @@ def data_stark():
                 heroe_mas_liviano = reduce_lista(lambda ant, act: act if ant['peso'] > act['peso'] else ant, lista_personajes)
                 print(f"El heroe más liviano es {heroe_mas_liviano["nombre"]} y pesa {heroe_mas_liviano["peso"]}kg.")
                 print()
-                                
+
+            case "I":
+                try:
+                    heroe_m = filtrar_lista(lambda heroe: heroe["genero"] == "M", lista_personajes)
+                    mostrar_heroes(heroe_m)
+                    print()
+
+                except ValueError as ex:
+                    print(ex)     
+
+            case "J":
+                try:
+                    heroe_f = filtrar_lista(lambda heroe: heroe["genero"] == "F", lista_personajes)
+                    mostrar_heroes(heroe_f)
+                    print()
+
+                except ValueError as ex:
+                    print(ex)
+
+            case "K":
+                mas_alto_masculino= reduce_lista(lambda ant, act: act if ant['altura'] < act['altura'] else ant, heroe_m)
+                mostrar_heroe(mas_alto_masculino)
+
+            case "L":
+                mas_alto_femenino= reduce_lista(lambda ant, act: act if ant['altura'] < act['altura'] else ant, heroe_f)
+                mostrar_heroe(mas_alto_femenino)
+
+            case "M":
+                mas_bajo_masculino= reduce_lista(lambda ant, act: act if ant['altura'] > act['altura'] else ant, heroe_m)
+                mostrar_heroe(mas_bajo_masculino)
+
+            case "N":
+                mas_bajo_femenino= reduce_lista(lambda ant, act: act if ant['altura'] > act['altura'] else ant, heroe_f)
+                mostrar_heroe(mas_bajo_femenino)
+
+            case "O":
+                campo = "a"
+                promedio_campo(heroe_m, campo)
+
+            case "P":
+                campo = "a"
+                promedio_campo(heroe_f, campo)
+
+            case "Q":
+                print(mas_alto_masculino["nombre"])
+                print(mas_alto_femenino["nombre"])
+                print(mas_bajo_masculino["nombre"])
+                print(mas_bajo_femenino["nombre"])
+                print()
+
+            case "R":
+                conteo_color_ojos = contar_elementos_por_clave(lista_personajes, "color_ojos")
+                print("Conteo por color de ojos:")
+                mostrar_lista_tuplas(conteo_color_ojos)
+
+            case "S":
+                conteo_color_pelo = contar_elementos_por_clave(lista_personajes, "color_pelo")
+                print("Conteo por color de pelo:")
+                mostrar_lista_tuplas(conteo_color_pelo)
+
+            case "T":
+                conteo_inteligencia = contar_elementos_por_clave(lista_personajes, "inteligencia")
+                print("Conteo por inteligencia:")
+                mostrar_lista_tuplas(conteo_inteligencia)
+
+            case "U":
+                agrupacion_color_ojos = agrupar_heroes_por_clave(lista_personajes, "color_ojos")
+                for valor in agrupacion_color_ojos:
+                    print(f"Agrupación por color de ojos: {valor[0]}")
+                    mostrar_heroes(valor[1])
+                    print()
+
+            case "V":
+                agrupacion_color_pelo = agrupar_heroes_por_clave(lista_personajes, "color_pelo")
+                for valor in agrupacion_color_pelo:
+                    print(f"Agrupación por color de pelo: {valor[0]}")
+                    mostrar_heroes(valor[1])
+                    print()
+
+            case "W":
+                agrupacion_inteligencia = agrupar_heroes_por_clave(lista_personajes, "inteligencia")
+                for valor in agrupacion_inteligencia:
+                    print(f"Agrupación por inteligencia: {valor[0]}")
+                    mostrar_heroes(valor[1])
+                    print()
+
             case "X":
                 break
 
